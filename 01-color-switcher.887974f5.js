3ad18d0ba4eeb9b0b10d6480efc3cee4f90245d5
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2022-10-30 10:02:20 +0000

    Deploying to gh-pages from @ Vlasova1983/goit-js-hw-09@91b2b219e73b26123f95f5e152b75030638aa871 🚀

---
+++ 01-color-switcher.887974f5.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=(document.querySelector("[data-stop]"),document.querySelector("body"));t.addEventListener("click",(function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));e.style.background=t}))}();
+//# sourceMappingURL=01-color-switcher.887974f5.js.map
