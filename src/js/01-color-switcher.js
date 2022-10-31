74422c6c3602ba781ce7a03b9eb3dc99a3a907a4
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2022-10-31 11:28:00 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -5,8 +5,8 @@ const backgroundColorBody = document.querySelector('body');
 let timerId = null;
 
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  }  
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}  
 
 startChangeBackgroundColorBtn.addEventListener("click", () => {
   timerId = setInterval(() => {
