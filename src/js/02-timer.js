061dace6a815258225fb1e2757b61242abbee51f
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2022-11-02 15:06:38 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -9,39 +9,37 @@ const timeLeft = {
     minutes: document.querySelector('[data-minutes]'),
     seconds: document.querySelector('[data-seconds]')
 };
+
 const options = {
     enableTime: true,
     time_24hr: true,
     defaultDate: new Date(),
     minuteIncrement: 1,
-    onClose(selectedDates) {
-      console.log(selectedDates[0]);
+    onClose(selectedDates) {      
+      day3 = selectedDates[0].getTime();
+      console.log(day3);
     },
   };
 let timerId = null;
-let currentTime={
-    data:options.defaultDate.getDate(),
-    hours:options.defaultDate.getHours(),
-    minutes:options.defaultDate.getMinutes(),
-    seconds:options.defaultDate.getSeconds()
-};
-console.log(currentTime);
-console.log(options.defaultDate);
+let day3 =null;
+const day1 = Date.now();
+console.log(day1);
+
+
 
 
 flatpickr("#datetime-picker", options);
 
 startChangeTimeBtn.addEventListener("click", () => {
-
-    //  timerId = setInterval(() => {
-        console.log(options.selectedDates.getDate())
-        const data = convertMs(options.defaultDate);
-         console.log(options.selectedDates[0].getDate())
-        // timeLeft.days.textContent = data.days;
-        // timeLeft.hours.textContent = data.hours;
-        // timeLeft.minutes.textContent = data.minutes;
-        // timeLeft.seconds.textContent = data.seconds;
-    //  }, 1000);
+    timerId = setInterval(() => { 
+        const day2 = Date.now();
+        console.log(day2);
+        const  day= convertMs(day3-day2);
+        timeLeft.days.textContent = day.days;
+        timeLeft.hours.textContent = day.hours;
+        timeLeft.minutes.textContent = day.minutes;
+        timeLeft.seconds.textContent = day.seconds;
+    }, 1000);
 });
   
 function convertMs(ms) {
@@ -63,6 +61,3 @@ function convertMs(ms) {
     return { days, hours, minutes, seconds };
 }
   
-//   console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-//   console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-//   console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
