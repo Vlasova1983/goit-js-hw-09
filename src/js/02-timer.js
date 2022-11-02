d03983e93934867de40ce17ea9d77dffeac5ddd9
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2022-11-02 15:21:46 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,44 +1,38 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
-
-const startChangeTimeBtn = document.querySelector('[data-start]');
-const input = document.querySelector('#datetime-picker');
+// const input = document.querySelector('#datetime-picker');
 const timeLeft = {
     days: document.querySelector('[data-days]'),
     hours: document.querySelector('[data-hours]'),
     minutes: document.querySelector('[data-minutes]'),
     seconds: document.querySelector('[data-seconds]')
 };
-
 const options = {
     enableTime: true,
     time_24hr: true,
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {      
-      day3 = selectedDates[0].getTime();
-      console.log(day3);
+        dataСhosen = selectedDates[0].getTime();
     },
-  };
+};
+
 let timerId = null;
-let day3 =null;
-const day1 = Date.now();
-console.log(day1);
-
-
+let dataСhosen =null;
+const today = Date.now();
 
 
 flatpickr("#datetime-picker", options);
+const startChangeTimeBtn = document.querySelector('[data-start]');
+
 
 startChangeTimeBtn.addEventListener("click", () => {
     timerId = setInterval(() => { 
-        const day2 = Date.now();
-        console.log(day2);
-        const  day= convertMs(day3-day2);
-        timeLeft.days.textContent = day.days;
-        timeLeft.hours.textContent = day.hours;
-        timeLeft.minutes.textContent = day.minutes;
-        timeLeft.seconds.textContent = day.seconds;
+        const  timer = convertMs(dataСhosen-Date.now());
+        timeLeft.days.textContent = timer.days;
+        timeLeft.hours.textContent = timer.hours;
+        timeLeft.minutes.textContent = timer.minutes;
+        timeLeft.seconds.textContent = timer.seconds;
     }, 1000);
 });
   
