b3726fda84012680acb2dbad6911cd3ea8f4a7ed
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2022-11-04 16:03:17 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,49 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const formEl={
+  form: document.querySelector('.form'),
+  delay: document.querySelector('[name ="delay"]'),
+  step: document.querySelector('[name="step"]'),
+  amount: document.querySelector('[name="amount"]'),
+}
+
+formEl.form.addEventListener("submit",onAmountValue);
+
+function onAmountValue(event){
+  event.preventDefault(); 
+  const amount = Number(formEl.amount.value);
+  const delay = Number(formEl.delay.value);
+  const step = Number(formEl.step.value);  
+  let sec = delay;
+
+  for(i=1; i<amount+1; i+=1){      
+    createPromise(i,sec)
+    .then(({ position, delay }) => {
+      Notiflix.Notify.info(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {        
+      Notiflix.Notify.info(`❌ Rejected promise ${position} in ${delay}ms`);
+    }) 
+    sec = sec + step;  
   }
 }
+
+function createPromise(position, delay) {  
+  return new Promise((resolve, reject) => {  
+    setTimeout(() => {      
+      if (Math.random() > 0.3) {                 
+        resolve ({position,delay});        
+      } else {        
+        reject({position,delay});
+      }
+    }, delay);  
+  }); 
+} 
+
+
+  
+
+
+
+
+  
