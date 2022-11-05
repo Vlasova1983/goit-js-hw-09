209ec99f71943a1d31b7e680a0cdc92e1b33fabd
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2022-11-05 15:14:52 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -3,12 +3,14 @@ const startChangeBackgroundColorBtn = document.querySelector('[data-start]');
 const stopChangeBackgroundColorBtn = document.querySelector('[data-stop]');
 const backgroundColorBody = document.querySelector('body');
 let timerId = null;
-
+stopChangeBackgroundColorBtn.setAttribute('disabled',true);
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }  
 
 startChangeBackgroundColorBtn.addEventListener("click", () => {
+  startChangeBackgroundColorBtn.setAttribute('disabled',true);
+  stopChangeBackgroundColorBtn.removeAttribute('disabled');
   timerId = setInterval(() => {
     const randomColor = getRandomHexColor();
     backgroundColorBody.style.background = randomColor;
@@ -17,5 +19,6 @@ startChangeBackgroundColorBtn.addEventListener("click", () => {
 
 stopChangeBackgroundColorBtn.addEventListener("click", () => {
   clearInterval(timerId);
- 
+  stopChangeBackgroundColorBtn.setAttribute('disabled',true);
+  startChangeBackgroundColorBtn.removeAttribute('disabled');
 });
